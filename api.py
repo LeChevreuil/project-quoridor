83b8a916953fa2f89941b17250460b41ba8156be
LeChevreuil <viber138@ulaval.ca>
2022-11-18 01:38:55 -0500

    Commit initial: creation des modules et des fonctions vides

---
+++ api.py
@@ -0,0 +1,13 @@
+
+
+def lister_parties():
+    pass
+
+def recuperer_partie():
+    pass
+
+def debuter_partie():
+    pass
+
+def jouer_coup():
+    pass
---
+++ quoridor.py
@@ -0,0 +1,18 @@
+
+def analyser_commande():
+    pass
+
+def formater_legende():
+    pass
+
+def formater_damier():
+    pass
+
+def formater_jeu():
+    pass
+
+def formater_les_parties():
+    pass
+
+def recuperer_le_coup():
+    pass
