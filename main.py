b96975ff349eaab827b4a098183541ab57f480b3
LeChevreuil <viber138@ulaval.ca>
2022-11-18 12:17:20 -0500

    Finition du module quoridor

---
+++ main.py
@@ -0,0 +1,33 @@
+import quoridor
+
+# Point d'entree
+quoridor.analyser_commande()
+
+DEBUG = {
+    "joueurs":  [
+        {"nom": "IDUL", "murs": 7, "pos": [5,5]},
+        {"nom": "automate", "murs": 3, "pos": [8,6]}
+    ],
+    "murs": {
+        "horizontaux": [[4, 4], [2, 6], [3, 8], [5, 8], [7, 8]],
+        "verticaux": [[6,2], [4,4],[2,6],[7,5],[7,7]]
+    }
+}
+
+PARTIE = [
+        {
+            "id": "5559cafd-6966-4465-af6f-67a784016b41",
+            "date": "2022-09-23 11:58:20",
+            "joueurs": ["IDUL", "automate"],
+            "gagnant": None
+        },
+        {
+            "id": "80a0a0d2-059d-4539-9d53-78b3f6045943",
+            "date": "2022-09-24 14:23:59",
+            "joueurs": ["IDUL", "automate"],
+            "gagnant": "automate"
+        }
+]
+
+print(quoridor.formater_jeu(DEBUG))
+print(quoridor.formater_les_parties(PARTIE))
--- quoridor.py
+++ quoridor.py
@@ -1,18 +1,82 @@
+import argparse
 
 def analyser_commande():
-    pass
+    parser = argparse.ArgumentParser(description="Quoridor")
 
-def formater_legende():
-    pass
+    parser.add_argument('idul', metavar='idul', type=str, help='IDUL du joueur')
+    parser.add_argument("-p", "--parties", help="Lister les parties existantes")
 
-def formater_damier():
-    pass
+    return parser.parse_args()
 
-def formater_jeu():
-    pass
+def formater_legende(joueurs):
+    resultat = "Légende:\n"
 
-def formater_les_parties():
-    pass
+    compte = 1
+    for joueur in joueurs:
+        murs = joueur["murs"]
+        resultat += "   " + str(compte) + "=" + joueur["nom"] + ",\tmurs=" + ("|" * int(murs)) + "\n"
+        compte += 1
+
+    return resultat
+
+def formater_damier(joueurs, murs):
+    padding = 3
+    lignes = ["   " + "-" * padding * 9]
+
+    # construire le damier
+    for y in range(9, 0, -1):
+        temp = str(y) + " |"
+        for x in range(1, 10):
+
+            # verifier si c'est un joueur
+            numJoueur = 1
+            joueurPlace = False
+            for joueur in joueurs:
+                if x == joueur["pos"][0] and y == joueur["pos"][1]:
+                    temp += " " + str(numJoueur) + " "
+                    joueurPlace = True
+                numJoueur += 1
+
+            # sinon
+            if not joueurPlace:
+                temp += " . "    
+
+        temp += "|" + "" 
+        lignes.append(temp) 
+
+        # verifier murs horizontaux
+        ligne = "  |" + (" " * 3 * 9) + "|"
+
+        lignes.append(ligne)
+
+    resulat = "\n".join(lignes) + "\n"
+    resulat += "--|" + "-" * padding * 9 + "\n"
+    resulat += "  |"
+
+    for i in range(1, 10):
+        resulat += " " + str(i) + " "
+
+    resulat += "\n"
+    return resulat
+
+def formater_jeu(etat_jeu):
+    return formater_legende(etat_jeu["joueurs"]) + formater_damier(etat_jeu["joueurs"], etat_jeu["murs"])
+
+def formater_les_parties(list_parties):
+    ligne = []
+    count = 1
+    print(list_parties)
+    for partie in list_parties:
+        temp = str(count) + (" : " if count <= 9 else ": ") + partie["date"] + ", " + partie["joueurs"][0] + " vs " + partie["joueurs"][1]
+        temp += ", gagnant: " + partie["gagnant"] if partie["gagnant"] is not None else ""
+        ligne.append(temp)
+        count += 1
+    return "\n".join(ligne)
+
 
 def recuperer_le_coup():
-    pass
+    print("Quel type de coup voulez-vous jouer? ('D', 'MH', 'MV): ")
+    type_coup = input()
+    print("Donnez la position où appliquer ce coup (x,y): ")
+    pos = input()
+    
